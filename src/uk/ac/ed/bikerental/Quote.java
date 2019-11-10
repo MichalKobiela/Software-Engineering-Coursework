abc25b57bad5a9a22b25be6212f4febfac6dcd3e
Tobias Rodriguez Del Pozo Wallin <s1807873@strange.inf.ed.ac.uk>
2019-11-10 20:21:27 +0000

    Implement Quote.java

--- src/uk/ac/ed/bikerental/Quote.java
+++ src/uk/ac/ed/bikerental/Quote.java
@@ -1,5 +1,47 @@
 package uk.ac.ed.bikerental;
 
-public class Quote {
+import java.util.Collection;
 
+public class Quote {
+    private BikeProvider bikeProvider;
+    private DateRange dateRange;
+    private double totalPrice;
+    private double deposit;
+    private Collection<Bike> bikes;
+    
+    public Quote(BikeProvider bikeProvider, DateRange dateRange, double totalPrice, double deposit,
+            Collection<Bike> bikes) {
+        super();
+        this.bikeProvider = bikeProvider;
+        this.dateRange = dateRange;
+        this.totalPrice = totalPrice;
+        this.deposit = deposit;
+        this.bikes = bikes;
+    }
+
+    public boolean reserveBikes() {
+        if (isEmpty()) {
+           for(Bike bike: bikes) {
+                bike.reserve(dateRange);
+           }
+           return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+    
+    public boolean isEmpty() {
+        return bikes.isEmpty();
+    }
+    
+    public void registerReturnOfBikes() {
+        for (Bike bike: bikes) {
+            bike.setInStore(true);
+        }
+    }
+    
+    
+    
 }
